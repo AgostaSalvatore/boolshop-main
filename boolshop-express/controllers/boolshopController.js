eb6847293d26313b290573050a656db5d7bbf687
AgostaSalvatore <agosta.boolean@gmail.com>
2025-06-14 11:19:32 +0200

    [upd] db connection done, index-show done, env fine on package.json

--- boolshop-express/controllers/boolshopController.js
+++ boolshop-express/controllers/boolshopController.js
@@ -1,15 +1,28 @@
 // Qui va la connessione al db
-// const connection = require('../data/db')
+const connection = require('../data/db')
 
 const index = (req, res) => {
-    console.log("elenco giochi")
+    connection.query('SELECT * FROM videogame', (err, result) => {
+        if (err) return res.status(500).json({ error: "Database Query Failed:" + err });
+        res.json(result);
+    })
 }
 
 const show = (req, res) => {
+    const { id } = req.params
     console.log("dettaglio gioco");
+    const recordSql = "SELECT * FROM videogame WHERE id = ?"
+    connection.query(recordSql, [id], (err, recordResult) => {
+        if (err) return res.status(500).json({ error: "Database Query Failed:" + err });
+
+        if (recordResult.length === 0) return res.status(404).json({ error: "Record not found" });
+
+        const record = recordResult[0]
+        res.json(record)
+    })
 }
 
-module.export = {
+module.exports = {
     index,
     show
 }
