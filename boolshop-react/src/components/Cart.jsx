0d6cb133440ae1c2eeabbca6f7a6dca2f4fb203d
DavideCotigliani <davide.cotigliani@gmail.com>
2025-06-16 18:01:06 +0200

    [upd] skeleton of default cart

--- boolshop-react/src/components/Cart.jsx
+++ boolshop-react/src/components/Cart.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Cart = () => {
   return (
     <div>
-      <h1>Cart</h1>
+      <div className="container my-3">
+        <h2>Il tuo carrello</h2>
+        <div className="row">
+          <div className="col d-flex">
+            <h4>Il carrelo è vuoto</h4>
+            <p> <a href="/" className='text-decoration-none'>Vai alla Homepage per trovare i prodotti che desideri</a></p>
+          </div>
+        </div>
+        <div className="col">
+          <h5>Riepilogo ordine</h5>
+          <div className="d-flex justify-content-between">
+            <span>Prodotti</span>
+            <span>€ 0,00</span>
+            <hr />
+            <div className="d-flex justify-content-between">
+              <span>Totale</span>
+              <span>0</span>
+            </div>
+            <hr />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
