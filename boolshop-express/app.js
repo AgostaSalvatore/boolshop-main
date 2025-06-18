2306a131617697fc248f8e1e643af25f291b9922
AgostaSalvatore <agosta.boolean@gmail.com>
2025-06-18 10:27:55 +0200

    [upd] add comment to app and router

--- boolshop-express/app.js
+++ boolshop-express/app.js
@@ -1,4 +1,5 @@
 const express = require("express"); //import express
+const cors = require('cors') //cors
 const boolRouter = require('./router/boolshopRouter'); // import router
 const notFound = require('./middlewares/notFound'); // import notFound
 const errorHandler = require('./middlewares/errorHandler'); // import errorHandler
@@ -8,17 +9,17 @@ const app = express(); //uso express
 
 const port = process.env.SERVER_PORT || 3000; //porta
 
-const cors = require('cors') //cors
-
 app.use(express.static("public")); //static files
 app.use(express.json()); //json
 app.use(setImagePath); //set image path
+
+// Configurazione CORS personalizzata per consentire le richieste dal frontend
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Origine del frontend
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Metodi consentiti
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Header consentiti
     res.header("Access-Control-Allow-Credentials", "true"); // Consenti credenziali
-    next();
+    next(); // Passa al prossimo middleware
 });
 
 //base route
@@ -26,12 +27,15 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
+// Utilizziamo il router per gestire tutte le richieste al percorso /api/boolShop
 app.use('/api/boolShop', boolRouter);
 
+// Middleware per gestire le richieste a percorsi non esistenti
 app.use(notFound);
+// Middleware per gestire gli errori generali dell'applicazione
 app.use(errorHandler);
 
-//listen
+// Avviamo il server sulla porta specificata
 app.listen(port, () => {
     console.log("Server started on port " + port);
 });
--- boolshop-express/router/boolshopRouter.js
+++ boolshop-express/router/boolshopRouter.js
@@ -1,12 +1,19 @@
+// Importiamo il modulo express per creare il router
 const express = require("express");
+// Creiamo un nuovo router Express
 const router = express.Router();
+// Importiamo il controller che contiene le funzioni per gestire le richieste
 const boolshopController = require("../controllers/boolshopController");
 
-//routes
+// Definiamo le rotte dell'API
+// Rotta per ottenere tutti i videogiochi
 router.get("/", boolshopController.index);
+// Rotta per ottenere i videogiochi ordinati per prezzo decrescente
 router.get("/price-desc", boolshopController.orderByPriceDesc);
+// Rotta per ottenere i videogiochi ordinati per prezzo crescente
 router.get("/price-asc", boolshopController.orderByPriceAsc);
+// Rotta per ottenere i dettagli di un singolo videogioco tramite il suo ID
 router.get("/:id", boolshopController.show);
 
-//export
+// Export
 module.exports = router;
